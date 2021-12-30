b6eeb65ea81b33cf13e300e71164cd87e8a0a6bd
Wilfried <la2spaille@gmail.com>
2021-12-30 01:42:11 +0100

    animation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const menuBtn = document.querySelector('.w-menu')
 const closeBtn = document.querySelector('.w-header-mobile-nav-close')
 const headerNavMobile = document.querySelector('.w-header-mobile-nav')
+const loader = document.querySelector('#loader')
+window.addEventListener('load', (e) => {
+    loader.classList.add('load')
+    e.stopPropagation()
+})
 menuBtn.addEventListener('click', (e) => {
     headerNavMobile.classList.add('is-active')
     e.stopPropagation()
@@ -9,3 +14,19 @@ closeBtn.addEventListener('click', (e) => {
     headerNavMobile.classList.remove('is-active')
     e.stopPropagation()
 })
+// var animElement = document.querySelectorAll('.anim-element')
+// var windowH = window.innerHeight
+// var scrollToop = window.scrollTop
+// var elementOffset = animElement.scrollTop
+// var distance = elementOffset - scrollToop
+// // window.addEventListener('load', (e) => {
+//     animElement.forEach(element => {
+//         window.addEventListener('scroll', (e) => {
+//             if (distance < 800){
+//                 element.classList.add('is-active')
+//             }
+//             e.stopPropagation()
+//         })
+//     })
+// //     e.stopPropagation()
+// // })
